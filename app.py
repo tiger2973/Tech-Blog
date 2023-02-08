615c1c4660427c559a6683674de89a51aadd2bac
tiger2973 <124491753+tiger2973@users.noreply.github.com>
2023-02-09 00:27:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import *
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = 'pymysql'+os.getenv("DATABASE URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
