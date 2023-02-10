f2967e3a2a691502043754a9c56b07e04644de9a
tiger2973 <124491753+tiger2973@users.noreply.github.com>
2023-02-10 15:09:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,50 +4,52 @@ import pymysql
 pymysql.install_as_MySQLdb()
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+try:
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db = SQLAlchemy(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+    class demo(db.Model):
+        _tablename__ = 'demo'
+        sno = db.Column(db.Integer, primary_key=True)
+        name = db.Column(db.String(20), nullable=False)
+        phone= db.Column(db.String(10), nullable=False)
+        message = db.Column(db.String(50), nullable=False)
+        email = db.Column(db.String(30), nullable=False)
 
-class demo(db.Model):
-    _tablename__ = 'demo'
-    sno = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20), nullable=False)
-    phone= db.Column(db.String(10), nullable=False)
-    message = db.Column(db.String(50), nullable=False)
-    email = db.Column(db.String(30), nullable=False)
-
-@app.route("/")
-def home():
-    return render_template('index.html')
-
-
-@app.route("/about")
-def about():
-    return render_template('about.html')
+    @app.route("/")
+    def home():
+        return render_template('index.html')
 
 
+    @app.route("/about")
+    def about():
+        return render_template('about.html')
 
-@app.route("/contact")
-def contact():
-    return render_template('contact.html')
 
-@app.route("/oncontact", methods = ['GET', 'POST'])
-def oncontact():
-    if(request.method=='POST'):
-        '''Add entry to the database'''
-        name = request.form.get('name')
-        email = request.form.get('email')
-        phone = request.form.get('phone')
-        message = request.form.get('message')
-        entry = demo(name=name, phone = phone, message = message,email = email)
-        db.session.add(entry)
-        db.session.commit()
-    return render_template('contact.html')
 
-@app.route("/post")
-def post():
-    return render_template('post.html')
+    @app.route("/contact")
+    def contact():
+        return render_template('contact.html')
 
-if __name__ == '__main__':  
-   app.run(host='0.0.0.0',debug = True)
+    @app.route("/oncontact", methods = ['GET', 'POST'])
+    def oncontact():
+        if(request.method=='POST'):
+            '''Add entry to the database'''
+            name = request.form.get('name')
+            email = request.form.get('email')
+            phone = request.form.get('phone')
+            message = request.form.get('message')
+            entry = demo(name=name, phone = phone, message = message,email = email)
+            db.session.add(entry)
+            db.session.commit()
+        return render_template('contact.html')
+
+    @app.route("/post")
+    def post():
+        return render_template('post.html')
+
+    if __name__ == '__main__':  
+       app.run(host='0.0.0.0',debug = True)
+except:
+    pass
